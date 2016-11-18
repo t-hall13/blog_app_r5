744d94cbf05c05c941ddec952e15ebd534046a7d
Tim Hall <timothypatrickhall@gmail.com>
2016-11-18 14:11:08 +0000

    Added validations to Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,9 +8,13 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-       @article.save
+      if @article.save
         flash[:success] = "Article has been created"
         redirect_to articles_path
+      else
+        flash[:danger] = "Article has not been created"
+        render 'new'
+      end
   end
   def article_params
     params.require(:article).permit(:title, :body)
--- spec/features/creating_articles_spec.rb
+++ spec/features/creating_articles_spec.rb
@@ -19,4 +19,21 @@ RSpec.feature "Creating Articles" do
     #Articles path
     expect(page.current_path).to eq(articles_path)
   end
+  scenario "A user fails to create a new article" do
+    #visit root page 
+     visit "/"
+    #click on new article link
+    click_link "New Article"
+    #fill in title
+    fill_in "Title", with: ""
+    #fill in body
+    fill_in "Body", with: ""
+    #create article
+    click_button "Create Article"
+    
+    expect(page).to have_content("Article has not been created")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
+    
+  end
 end
