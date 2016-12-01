bb4535bae8d3279d1ab876f32388372ebfe51277
Tim Hall <timothypatrickhall@gmail.com>
2016-12-01 04:27:43 +0000

    Enforce sign in before posting comments

---
+++ spec/requests/comments_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "Comments", type: :request do
+  before do 
+    @john = User.create(email: "john@example.com", password: "password")
+    @paul = User.create(email: "paul@example.com", password: "password")
+    @article = Article.create!(title: "Article One", body: "Body of Article One", user: @john)
+  end
+  describe 'POST /articles/:id/comments' do
+    context 'with a  non signed in user' do
+      before do
+       post "/articles/#{@article.id}/comments", params: {comment: {body: "Awesome blog"}}
+      end
+      it 'redirects the user to the sign in page' do
+        flash_message = "Please sign in or sign up first"
+        expect(response).to redirect_to(new_user_session_path)
+        expect(response.status).to eq 302
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+   
+    context 'with a logged in user' do
+      before do
+       login_as(@paul)
+       post "/articles/#{ @article.id }/comments", params: { comment: {body: "Awesome blog" }}
+      end
+      
+      it "creates the comment successfully" do
+        flash_message = "Comment has been created"
+        expect(response).to redirect_to(article_path(@article.id))
+        expect(response.status).to eq 302
+        expect(flash[:notice]).to eq flash_message
+      end
+    end
+    
+  
+    
+  end
+end
